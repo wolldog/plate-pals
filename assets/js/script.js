9f09262498191148d2803f8108335ddc67e3b6c0
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-01-03 20:08:56 +1100

    Add API base url and fetch statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-hello
+const apiUrl = `https://www.themealdb.com/api/json/v2/9973533/${search}`
+
+let search = ``
+
+fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayMeals(data)
+        
+      });
+    } else { 
+      alert('Error: ' + response.statusText);
+    }
+  });
