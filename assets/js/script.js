996dbf869e12406bf0f5e1d8fd3799cafa71f819
mnabiei1379 <mnabiei1379@gmail.com>
2023-01-04 11:08:22 +0330

    added a filter by category and main ingredient click event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 
-var foodbyOriginBtn = document.querySelector('#origin-filter')
+var foodbyOriginBtn = document.querySelector('#origin-filter');
+var foodbyIngredientBtn = document.querySelector('#ingredient-filter');
+var foodbyCategoryBtn = document.querySelector('#category-filter');
+
 
 let search = `randomselection.php`
 
@@ -55,12 +58,71 @@ fetch(apiUrl).then(function (response) {
 
   // Add click listeners for the food recipe and cocktail recipe refinement options
   foodbyOriginBtn.addEventListener ("click", refineByOrigin );
+  foodbyIngredientBtn.addEventListener ("click", refineByIngredients );
+  foodbyCategoryBtn.addEventListener ("click", refineByCategory  );
 
 
 
   // Create a function that will dynamically refine the list of dispalyed based on the origin/area
   function refineByOrigin(recipies) {
+    // Here I wanna remove the card container entirely from the page when the function is called
+    // I get errors here so I commented out:
+    
+    // cardContainer.style.display = "none";
+    // cardBodyDivEl.style.display = "none";
+    // cardTitleEl.style.display = "none";
+    // colDivEl.style.display = "none";
+    // cardDivEl.style.display = "none";
+    // cardImg.style.display = "none";
+
+
     apiUrl = `https://www.themealdb.com/api/json/v2/9973533/${filterByArea}`
+
+
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            displayMeals(data)
+            console.log("data after event click:");
+            console.log(data);
+            
+          });
+        } else { 
+          alert('Error: ' + response.statusText);
+        }
+      });
+      // call display meals function
+      console.log("recepies after event click:");
+      console.log(recipies);
+      displayMeals(recipies);
+  }
+
+  function refineByIngredients (recipies) {
+    apiUrl = `https://www.themealdb.com/api/json/v2/9973533/${filterByIngredient}`
+
+
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            displayMeals(data)
+            console.log("data after event click:");
+            console.log(data);
+            
+          });
+        } else { 
+          alert('Error: ' + response.statusText);
+        }
+      });
+      // call display meals function
+      console.log("recepies after event click:");
+      console.log(recipies);
+      displayMeals(recipies);
+  }
+
+  function refineByCategory (recipies) {
+    apiUrl = `https://www.themealdb.com/api/json/v2/9973533/${filterByCategory}`
 
 
 
