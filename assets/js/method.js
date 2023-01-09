fbe4dcd24f89ce27e7f4f326ac8c1700ac0cc895
mnabiei1379 <mnabiei1379@gmail.com>
2023-01-09 09:20:43 +0330

    Fixed the bug that cocktails recipes were not displaying

--- assets/js/method.js
+++ assets/js/method.js
@@ -5,7 +5,12 @@ var recipeId = (localStorage.getItem("methodId"))
 
 var source
 
+var ingredient
+
 const methodApi = `https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=${recipeId}`
+
+const drinksApi = `https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i=${recipeId}`
+
 
 fetch(methodApi).then(function (response) {
     if (response.ok) {
@@ -19,16 +24,39 @@ fetch(methodApi).then(function (response) {
     }
   });
 
-function displayMethod (){
+  fetch(drinksApi).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        ingredient = (data.drinks[0]);
+        displayFavDrink ();
+        
+      });
+    } else { 
+      alert('Error: ' + response.statusText);
+    }
+  });
+
+
+function displayMethod () {
   
   document.getElementById("recipeTitle").innerHTML = source.strMeal;
+
   document.getElementById("recipeImg").src = source.strMealThumb;
+
   document.getElementById("recipeMethod").innerHTML = source.strIngredient1;
   document.getElementById("recipeIngredients").innerHTML = source.strInstructions;
   console.log(source)
 
+}
 
-
+function displayFavDrink () {
+  document.getElementById("recipeTitle").innerHTML = ingredient.strDrink;
+  document.getElementById("recipeImg").src = ingredient.strDrinkThumb;
+  document.getElementById("recipeMethod").innerHTML = ingredient.strIngredient1;
+  document.getElementById("recipeIngredients").innerHTML = ingredient.strInstructions;
+  // Drink API
+  console.log("Drink source")
+  console.log(ingredient)
 }
 
 TitleBtn.addEventListener("click", toMainPage);
